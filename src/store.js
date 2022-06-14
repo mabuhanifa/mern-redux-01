b98bbd15272a3dfad55a0d512632f765d944f5d8
shourovahmd <moh.abuhanifa@gmail.com>
2022-06-15 03:34:50 +0600

    cart-reducer added

--- src/store.js
+++ src/store.js
@@ -1,11 +1,13 @@
 import { applyMiddleware, combineReducers, createStore } from "redux";
 import { composeWithDevTools } from "redux-devtools-extension";
 import thunk from "redux-thunk";
+import { cartReducer } from "./reducers/cartReducer";
 import { productDetailsReducer, productListReducer } from "./reducers/productReducers";
 
 const reducer = combineReducers({
   productList: productListReducer,
    productDetails: productDetailsReducer,
+   cart : cartReducer,
 });
 const initialState = {};
 const middleware = [thunk];
