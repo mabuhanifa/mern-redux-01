81f97348adc5f1cc0cb5a9ab7669beb7e6df6fd3
shourovahmd <moh.abuhanifa@gmail.com>
2022-06-15 00:48:57 +0600

    actions done

--- src/actions/productActions.js
+++ src/actions/productActions.js
@@ -6,18 +6,17 @@ import {
 } from "../constants/productConstants";
 
 export const listProducts = () => async (dispatch) => {
-  
-    try {
+  try {
     dispatch({ type: PRODUCT_LIST_REQUEST });
     const { data } = await axios.get("api/products");
     dispatch({ type: PRODUCT_LIST_SUCCESS, payload: data });
-  } 
-  catch (error) {
-    dispatch({ type: PRODUCT_LIST_FAIL,
-         payload: error.response && error.response.data.message
+  } catch (error) {
+    dispatch({
+      type: PRODUCT_LIST_FAIL,
+      payload:
+        error.response && error.response.data.message
           ? error.response.data.message
-            : error.message
-        
-        });
+          : error.message,
+    });
   }
 };
