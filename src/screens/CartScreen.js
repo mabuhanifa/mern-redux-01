a7237e34ba10a713d2915e1b9abb86206e3604f9
shourovahmd <moh.abuhanifa@gmail.com>
2022-06-15 16:31:49 +0600

    checkout added

--- src/screens/CartScreen.js
+++ src/screens/CartScreen.js
@@ -1,5 +1,14 @@
 import { useEffect } from "react";
-import { Col, Container, Image, ListGroup, Row } from "react-bootstrap";
+import {
+    Button,
+    Card,
+    Col,
+    Container,
+    Form,
+    Image,
+    ListGroup,
+    Row
+} from "react-bootstrap";
 import { useDispatch, useSelector } from "react-redux";
 import { Link, useLocation, useNavigate, useParams } from "react-router-dom";
 import { addToCart } from "../actions/cartActions";
@@ -13,16 +22,20 @@ const CartScreen = () => {
   const dispatch = useDispatch();
   const cart = useSelector((state) => state.cart);
   const { cartItems } = cart;
-  console.log(cartItems);
 
   useEffect(() => {
     if (id) {
       dispatch(addToCart(id, qty));
     }
   }, [dispatch, id, qty]);
-
+  const removeFromCartHandler = (productId) => {
+    console.log("remove");
+  };
+  const checkoutHandler = () => {
+    navigate("/signin?redirect=/checkout");
+  }
   return (
-    <Container>
+    <Container className="mt-3">
       <Row>
         <Col md={8}>
           <h1>Shopping Cart</h1>
@@ -33,19 +46,37 @@ const CartScreen = () => {
           ) : (
             <ListGroup variant="flush">
               {cartItems.map((item) => (
-                <ListGroup.Item key={item.id}>
+                <ListGroup.Item key={id}>
                   <Row>
                     <Col md={2}>
                       <Image src={item.image} alt={item.name} fluid rounded />
                     </Col>
                     <Col md={3}>
-                        <Link to={`/product/${id}`}>{item.name}</Link>
+                      <Link to={`/product/${id}`}>{item.name}</Link>
                     </Col>
+                    <Col md={2}>{item.price}</Col>
                     <Col md={2}>
-                        {item.price}
+                      <Form.Control
+                        as="select"
+                        value={item.qty}
+                        onChange={(e) =>
+                          dispatch(addToCart(id, Number(e.target.value)))
+                        }
+                      >
+                        {[...Array(item.countInStock).keys()].map((x) => (
+                          <option key={x + 1} value={x + 1}>
+                            {x + 1}
+                          </option>
+                        ))}
+                      </Form.Control>
                     </Col>
-                    <Col md={2}>
-                        {item.qty}
+                    <Col md={1}>
+                      <Button
+                        variant="light"
+                        onClick={() => dispatch(removeFromCartHandler(id))}
+                      >
+                        <i className="fas fa-trash"></i> Remove
+                      </Button>
                     </Col>
                   </Row>
                 </ListGroup.Item>
@@ -53,9 +84,33 @@ const CartScreen = () => {
             </ListGroup>
           )}
         </Col>
-        <Col md={2}></Col>
-
-        <Col md={2}></Col>
+        <Col md={4}>
+          <Card>
+            <ListGroup variant="flush">
+              <ListGroup.Item>
+                <h2>
+                  Subtotal ({cartItems.reduce((acc, item) => acc + item.qty, 0)}{" "}
+                  items)
+                </h2>
+                ${" "}
+                {cartItems
+                  .reduce((acc, item) => acc + item.price * item.qty, 0)
+                  .toFixed(2)}
+              </ListGroup.Item>
+              <ListGroup.Item>
+                <Button
+                  type="button"
+                  className="w-100"
+                  variant="success"
+                  disabled={cartItems.length === 0}
+                  onClick={checkoutHandler}
+                >
+                 Proceed To  Checkout
+                </Button>
+              </ListGroup.Item>
+            </ListGroup>
+          </Card>
+        </Col>
       </Row>
     </Container>
   );
