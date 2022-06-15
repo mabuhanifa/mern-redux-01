1c77632eb76e0f0258064aacbe9180aedad8f34a
shourovahmd <moh.abuhanifa@gmail.com>
2022-06-16 02:25:34 +0600

    user added to local storage

--- src/store.js
+++ src/store.js
@@ -13,12 +13,16 @@ const reducer = combineReducers({
 });
 
 const cartItemsFromStorage = localStorage.getItem("cartItems")? JSON.parse(localStorage.getItem("cartItems")) : [];
+const userInfoFromStorage = localStorage.getItem("userInfo")? JSON.parse(localStorage.getItem("userInfo")) : [];
 
 
 const initialState = {
     cart: {
         cartItems: cartItemsFromStorage,
     },
+    userLogin: {
+        userInfo: userInfoFromStorage,
+    }
 };
 const middleware = [thunk];
 const store = createStore(
