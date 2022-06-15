06514c28eb4f43ac08bf37876321737c0a5fb0dc
shourovahmd <moh.abuhanifa@gmail.com>
2022-06-15 15:44:35 +0600

    working on cart

--- src/actions/cartActions.js
+++ src/actions/cartActions.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 import { CARD_ADD_ITEM } from "../constants/cartConstants";
 
-export const addToCart = (id, qty) => async (dispatch) => {
+export const addToCart = (id, qty) => async (dispatch, getState) => {
     const { data } = await axios.get(`http://localhost:5000/api/products/${id}`,);
     dispatch({ 
         type: CARD_ADD_ITEM, 
--- src/screens/CartScreen.js
+++ src/screens/CartScreen.js
@@ -1,11 +1,64 @@
-import { Container } from "react-bootstrap";
+import { useEffect } from "react";
+import { Col, Container, Image, ListGroup, Row } from "react-bootstrap";
+import { useDispatch, useSelector } from "react-redux";
+import { Link, useLocation, useNavigate, useParams } from "react-router-dom";
+import { addToCart } from "../actions/cartActions";
+import Message from "../components/Message";
 
 const CartScreen = () => {
-    return (
-        <Container>
-            Cart
-        </Container>
-    );
+  const { id } = useParams();
+  const navigate = useNavigate();
+  const location = useLocation();
+  const qty = location.search ? Number(location.search.split("=")[1]) : 1;
+  const dispatch = useDispatch();
+  const cart = useSelector((state) => state.cart);
+  const { cartItems } = cart;
+  console.log(cartItems);
+
+  useEffect(() => {
+    if (id) {
+      dispatch(addToCart(id, qty));
+    }
+  }, [dispatch, id, qty]);
+
+  return (
+    <Container>
+      <Row>
+        <Col md={8}>
+          <h1>Shopping Cart</h1>
+          {cartItems.length === 0 ? (
+            <Message>
+              Your Cart is empty <Link to="/">Go back</Link>
+            </Message>
+          ) : (
+            <ListGroup variant="flush">
+              {cartItems.map((item) => (
+                <ListGroup.Item key={item.id}>
+                  <Row>
+                    <Col md={2}>
+                      <Image src={item.image} alt={item.name} fluid rounded />
+                    </Col>
+                    <Col md={3}>
+                        <Link to={`/product/${id}`}>{item.name}</Link>
+                    </Col>
+                    <Col md={2}>
+                        {item.price}
+                    </Col>
+                    <Col md={2}>
+                        {item.qty}
+                    </Col>
+                  </Row>
+                </ListGroup.Item>
+              ))}
+            </ListGroup>
+          )}
+        </Col>
+        <Col md={2}></Col>
+
+        <Col md={2}></Col>
+      </Row>
+    </Container>
+  );
 };
 
 export default CartScreen;
