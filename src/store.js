0e906bcbafe786813336c4a6d505e926d9321abb
shourovahmd <moh.abuhanifa@gmail.com>
2022-06-16 02:21:24 +0600

    user reducer added

--- src/store.js
+++ src/store.js
@@ -3,11 +3,13 @@ import { composeWithDevTools } from "redux-devtools-extension";
 import thunk from "redux-thunk";
 import { cartReducer } from "./reducers/cartReducer";
 import { productDetailsReducer, productListReducer } from "./reducers/productReducers";
+import { userLoginReducer } from "./reducers/userReducer.js";
 
 const reducer = combineReducers({
   productList: productListReducer,
    productDetails: productDetailsReducer,
    cart : cartReducer,
+    userLogin: userLoginReducer
 });
 
 const cartItemsFromStorage = localStorage.getItem("cartItems")? JSON.parse(localStorage.getItem("cartItems")) : [];
