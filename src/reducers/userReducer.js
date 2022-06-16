77faf046b55bbcb645a6bea085a91501d292e480
shourovahmd <moh.abuhanifa@gmail.com>
2022-06-16 18:14:09 +0600

    user details processig

--- src/reducers/userReducer.js
+++ src/reducers/userReducer.js
@@ -1,4 +1,7 @@
 import {
+  USER_DETAILS_FAIL,
+  USER_DETAILS_REQUEST,
+  USER_DETAILS_SUCCESS,
   USER_LOGIN_FAIL,
   USER_LOGIN_REQUEST,
   USER_LOGIN_SUCCESS,
@@ -36,3 +39,20 @@ export const userRegisterReducer = (state = {}, action) => {
       return state;
   }
 };
+
+export const userDetailsReducer = (state = {
+  user: {},
+}, action) => {
+  switch (action.type) {
+    case USER_DETAILS_REQUEST:
+      return { loading: true};
+    case USER_DETAILS_SUCCESS:
+      return { loading: false, userInfo: action.payload };
+    case USER_DETAILS_FAIL:
+      return { loading: false, error: action.payload };
+
+    default:
+      return state;
+  }
+};
+
