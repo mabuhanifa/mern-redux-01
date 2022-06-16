d948886f60afb350dfa9bc3ce4204886d9f25714
shourovahmd <moh.abuhanifa@gmail.com>
2022-06-16 16:35:16 +0600

    sign in functionality done

--- src/actions/userActions.js
+++ src/actions/userActions.js
@@ -2,7 +2,8 @@ import axios from "axios";
 import {
   USER_LOGIN_FAIL,
   USER_LOGIN_REQUEST,
-  USER_LOGIN_SUCCESS
+  USER_LOGIN_SUCCESS,
+  USER_LOGOUT
 } from "../constants/userConstants";
 
 export const login = (email, password) => async (dispatch) => {
@@ -37,3 +38,10 @@ export const login = (email, password) => async (dispatch) => {
          });
   }
 };
+
+export const logout = () => (dispatch) => {
+  localStorage.removeItem("userInfo");
+  dispatch({
+    type: USER_LOGOUT,
+  });
+}
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,6 +1,14 @@
-import { Container, Nav, Navbar } from "react-bootstrap";
+import { Container, Nav, Navbar, NavDropdown } from "react-bootstrap";
+import { useDispatch, useSelector } from "react-redux";
 import { LinkContainer } from "react-router-bootstrap";
+import { logout } from '../actions/userActions';
 const Header = () => {
+  const dispatch = useDispatch();
+  const userLogin = useSelector((state) => state.userLogin);
+  const { userInfo } = userLogin;
+  const logoutHandler = () => {
+    dispatch(logout());
+  }
   return (
     <header>
       <Navbar bg="dark" variant="dark" expand="lg" collapseOnSelect>
@@ -16,10 +24,27 @@ const Header = () => {
                 <i className="fas fa-shopping-cart"></i>Cart
                 </Nav.Link>
               </LinkContainer>
-              <LinkContainer className="text-decoration-none" to="/login">
-              <Nav.Link >
-              <i className="fas fa-user"></i>sign In</Nav.Link>
-              </LinkContainer>
+              { userInfo ? (
+                <NavDropdown title={userInfo.name} id="basic-nav-dropdown">
+                  <LinkContainer className="text-decoration-none" to="/profile">
+                  <NavDropdown.Item >
+                      <i className="fas fa-user"></i>Profile
+                      </NavDropdown.Item>
+                    </LinkContainer>
+                  <NavDropdown.Divider />
+                  <NavDropdown.Item onClick={logoutHandler}>
+                  
+                    <i className="fas fa-sign-out-alt"></i>Logout
+                  </NavDropdown.Item>
+                </NavDropdown>
+              ) : (
+                <LinkContainer className="text-decoration-none" to="/login">
+                <Nav.Link >
+                    <i className="fas fa-sign-in-alt"></i>Signin
+                    </Nav.Link>
+                  </LinkContainer>
+
+                          )}
             </Nav>
           </Navbar.Collapse>
         </Container>
