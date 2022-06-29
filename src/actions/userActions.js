2a41551cbb5bd939f4d0341571bb3bed4f68e0b8
shourovahmd <moh.abuhanifa@gmail.com>
2022-06-29 15:38:01 +0600

    user list screen

--- src/actions/userActions.js
+++ src/actions/userActions.js
@@ -5,6 +5,9 @@ import {
   USER_DETAILS_REQUEST,
   USER_DETAILS_RESET,
   USER_DETAILS_SUCCESS,
+  USER_LIST_FAIL,
+  USER_LIST_REQUEST,
+  USER_LIST_SUCCESS,
   USER_LOGIN_FAIL,
   USER_LOGIN_REQUEST,
   USER_LOGIN_SUCCESS,
@@ -157,7 +160,7 @@ export const updateUserProfile = (user) => async (dispatch, getState) => {
       type: USER_UPDATE_PROFILE_SUCCESS,
       payload: data,
     });
-  } catch (error){
+  } catch (error) {
     dispatch({
       type: USER_UPDATE_PROFILE_FAIL,
       payload:
@@ -179,4 +182,41 @@ export const logout = () => (dispatch) => {
   dispatch({
     type: ORDER_LIST_MY_RESET,
   });
+};
+
+export const listUsers = () => async (dispatch, getState) => {
+  try {
+    dispatch({
+      type: USER_LIST_REQUEST,
+    });
+
+    const {
+      userLogin: { userInfo },
+    } = getState();
+
+    const config = {
+      headers: {
+        Authorization: `Bearer ${userInfo.token}`,
+      },
+    };
+
+    const { data } = await axios.get(`http://localhost:5000/api/users`, config);
+
+    dispatch({
+      type: USER_LIST_SUCCESS,
+      payload: data,
+    });
+  } catch (error) {
+    const message =
+      error.response && error.response.data.message
+        ? error.response.data.message
+        : error.message;
+    if (message === "Not authorized, token failed") {
+      dispatch(logout());
+    }
+    dispatch({
+      type: USER_LIST_FAIL,
+      payload: message,
+    });
+  }
 };
---
+++ src/screens/UserListScreen.js
@@ -0,0 +1,12 @@
+import { useDispatch } from 'react-redux';
+
+const UserListScreen = () => {
+    const dispatch = useDispatch();
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default UserListScreen;
