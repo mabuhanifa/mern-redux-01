a29f77f40776217d3df58e5c86fdc87e65ce1e96
shourovahmd <moh.abuhanifa@gmail.com>
2022-07-04 01:03:04 +0600

    productcarousel added

---
+++ src/components/ProductCarousel.js
@@ -0,0 +1,6 @@
+
+const ProductCarousel = () => {
+  return <div></div>;
+};
+
+export default ProductCarousel;
--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -4,15 +4,18 @@ import { useDispatch, useSelector } from "react-redux";
 import { useParams } from "react-router-dom";
 import { listProducts } from "../actions/productActions";
 import Message from "../components/Message";
+import Paginate from "../components/Paginate";
 import Product from "../components/Product";
 
 const HomeScreen = () => {
   const { pageNumber } = useParams() || 1;
   const { keyword } = useParams();
+  console.log(pageNumber, keyword);
   const dispatch = useDispatch();
 
   const productList = useSelector((state) => state.productList);
-  const { products, loading, error } = productList;
+  const { products, loading, error, page, pages } = productList;
+  console.log(products);
 
   useEffect(() => {
     dispatch(listProducts(keyword, pageNumber));
@@ -26,13 +29,20 @@ const HomeScreen = () => {
       ) : error ? (
         <Message variant="danger">{error}</Message>
       ) : (
-        <Row>
-          {products?.map((product) => (
-            <Col key={product._id} sm={12} md={6} lg={4} xl={3}>
-              <Product product={product} />
-            </Col>
-          ))}
-        </Row>
+        <>
+          <Row>
+            {products.products?.map((product) => (
+              <Col key={product._id} sm={12} md={6} lg={4} xl={3}>
+                <Product product={product} />
+              </Col>
+            ))}
+          </Row>
+          <Paginate
+            pages={pages}
+            page={page}
+            keyword={keyword ? keyword : ""}
+          />
+        </>
       )}
     </Container>
   );
