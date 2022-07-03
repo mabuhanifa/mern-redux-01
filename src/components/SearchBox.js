b9c727729c7967418298e4486890044d1b7d5af5
shourovahmd <moh.abuhanifa@gmail.com>
2022-07-03 21:15:29 +0600

    search box added

---
+++ src/components/SearchBox.js
@@ -0,0 +1,16 @@
+import { useState } from "react";
+import { Form } from "react-bootstrap";
+
+const SearchBox = () => {
+    const [keyword,setKeyword] = useState("");
+    return (
+        <div>
+            <Form>
+
+            </Form>
+            
+        </div>
+    );
+};
+
+export default SearchBox;
